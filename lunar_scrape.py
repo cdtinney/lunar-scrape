5cb6cd94d93059fd8340be9e959953ed49ed5e72
ctinney <ctinney@rossvideo.com>
2018-06-15 17:21:24 -0400

    Add script argument for preserving directory structure
    It now defaults to a flat structure where the paths are converted to file names
    via underscores (e.g. foo/bar/baz.png becomes foo_bar_baz.png).
    
    The original directory structure is huge and cumbersome to navigate.
    
    * Rename script (typo)

---
+++ lunar_scrape.py
@@ -0,0 +1,42 @@
+import os
+import urllib2
+import pathlib2
+import argparse
+from multiprocessing.dummy import Pool
+
+parser = argparse.ArgumentParser(description='Scrape lunar images from NASA.')
+parser.add_argument('--preserve-dir', dest='preserve_dir', action='store_true',
+                    help='Preserve directory')
+args = parser.parse_args()
+
+root_url = 'https://pds-imaging.jpl.nasa.gov/data/lo/LO_1001/EXTRAS/BROWSE/'
+default_base_dir = 'images'
+
+image_paths_file = open('image_paths.txt', 'r')
+image_paths = [line.rstrip('\n') for line in image_paths_file.readlines()]
+image_paths_file.close()
+
+def download_image(url, image_path):
+    return urllib2.urlopen(url + '/' + image_path).read()
+
+def get_save_path(base_dir, preserve_dir, image_path):
+  if preserve_dir:
+    return base_dir + '/' + image_path
+  else:
+    return base_dir + '/' + image_path.replace('/', '_')
+
+def save_image(base_dir, preserve_dir, image_path, image):
+    save_path = get_save_path(base_dir, preserve_dir, image_path)
+    save_path_parent_dir = os.path.dirname(save_path)
+    pathlib2.Path(save_path_parent_dir).mkdir(parents=True, exist_ok=True)
+    image_file = open(save_path, 'wb')
+    image_file.write(image)
+    image_file.close()
+
+def download_and_save_image(image_path):
+    save_image(default_base_dir, args.preserve_dir, image_path, download_image(root_url, image_path))
+
+def lunar_scrape():
+    Pool(4).map(download_and_save_image, image_paths)
+
+lunar_scrape()
